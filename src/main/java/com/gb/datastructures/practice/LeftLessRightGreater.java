5d58381212380bd33ac0ad993c95c465542886b9
g.balasubramanian <balasubramaniangopalakrishnan@gmail.com>
2019-02-13 00:38:50 +0530

    modified algorithm slightly

--- src/main/java/com/gb/datastructures/practice/LeftLessRightGreater.java
+++ src/main/java/com/gb/datastructures/practice/LeftLessRightGreater.java
@@ -6,21 +6,27 @@ package com.gb.datastructures.practice;
 public class LeftLessRightGreater {
     int findPos(int[] arr) {
         int[] leftMax = new int[arr.length];
+        int[] rightMin = new int[arr.length];
         printArray(arr);
         leftMax[0] = Integer.MIN_VALUE;
         for (int i = 1; i < arr.length; i++) {
             leftMax[i] = Math.max(arr[i - 1], leftMax[i - 1]);
         }
+        rightMin[arr.length-1] = Integer.MAX_VALUE;
+        for (int i = arr.length - 2; i >= 0; i--) {
+            rightMin[i] = Math.min(arr[i+1], rightMin[i+1]);
+        }
         printArray(leftMax);
-        int rightMin = Integer.MAX_VALUE;
-        for (int i = arr.length - 1; i >= 0; i--) {
 
-            if (leftMax[i] < arr[i] && rightMin > arr[i]) {
+        for (int i = 1; i <arr.length-1; i++) {
+
+            if (leftMax[i] <= arr[i] && rightMin[i] >= arr[i]) {
                 System.out.println(arr[i]);
                 //return arr[i];
+                break;
             }
-            rightMin = Math.min(arr[i], rightMin);
         }
+
         return -1;
 
     }
@@ -33,7 +39,9 @@ public class LeftLessRightGreater {
     }
 
     public static void main(String[] args) {
-        int[] arr = {4, 3, 2, 7, 8, 9}; //{5, 1, 4, 3, 6, 8, 10, 7, 9};
+        int[] arr = {7, 14, 16, 18};
+                //{4, 3, 2, 7, 8, 9};
+                 //{5, 1, 4, 3, 6, 8, 10, 7, 9};
         LeftLessRightGreater leftLessRightGreater = new LeftLessRightGreater();
         leftLessRightGreater.findPos(arr);
     }
