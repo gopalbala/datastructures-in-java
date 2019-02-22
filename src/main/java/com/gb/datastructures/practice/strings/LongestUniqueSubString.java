8fc28125a9f09480c1a39279ad882e18072bd372
g.balasubramanian <balasubramaniangopalakrishnan@gmail.com>
2019-02-22 23:59:08 +0530

    minorrefactoring

--- src/main/java/com/gb/datastructures/practice/strings/LongestUniqueSubString.java
+++ src/main/java/com/gb/datastructures/practice/strings/LongestUniqueSubString.java
@@ -8,36 +8,30 @@ import java.util.Arrays;
 public class LongestUniqueSubString {
     static final int NO_OF_CHARS = 256;
     public static int lengthOfLongestSubstring(String s) {
-        int n = s.length();
-        int cur_len = 1;    // length of current substring
-        int max_len = 1;    // result
-        int prev_index;        //  previous index
+        int curLen = 1;    // length of current substring
+        int maxLen = 1;    // result
+        int prevIndex;        //  previous index
         int i;
         int visited[] = new int[NO_OF_CHARS];
 
-        /* Initialize the visited array as -1, -1 is
-         used to indicate that character has not been
-         visited yet. */
         for (i = 0; i < NO_OF_CHARS; i++) {
             visited[i] = -1;
         }
 
-        /* Mark first character as visited by storing the
-             index of first   character in visited array. */
         visited[s.charAt(0)] = 0;
 
         /* Start from the second character. First character is
-           already processed (cur_len and max_len are initialized
+           already processed (curLen and maxLen are initialized
          as 1, and visited[str[0]] is set */
-        for(i = 1; i < n; i++)
+        for(i = 1; i < s.length(); i++)
         {
-            prev_index = visited[s.charAt(i)];
+            prevIndex = visited[s.charAt(i)];
 
             /* If the current character is not present in
            the already processed substring or it is not
-              part of the current NRCS, then do cur_len++ */
-            if(prev_index == -1 || i - cur_len > prev_index)
-                cur_len++;
+              part of the current NRCS, then do curLen++ */
+            if(prevIndex == -1 || i - curLen > prevIndex)
+                curLen++;
 
             /* If the current character is present in currently
                considered NRCS, then update NRCS to start from
@@ -46,24 +40,24 @@ public class LongestUniqueSubString {
             {
                 /* Also, when we are changing the NRCS, we
                    should also check whether length of the
-                   previous NRCS was greater than max_len or
+                   previous NRCS was greater than maxLen or
                    not.*/
-                if(cur_len > max_len)
-                    max_len = cur_len;
+                if(curLen > maxLen)
+                    maxLen = curLen;
 
-                cur_len = i - prev_index;
+                curLen = i - prevIndex;
             }
 
             // update the index of current character
             visited[s.charAt(i)] = i;
         }
 
-        // Compare the length of last NRCS with max_len and
-        // update max_len if needed
-        if(cur_len > max_len)
-            max_len = cur_len;
+        // Compare the length of last NRCS with maxLen and
+        // update maxLen if needed
+        if(curLen > maxLen)
+            maxLen = curLen;
 
-        return max_len;
+        return maxLen;
     }
 
     public static void main(String[] args) {
