5aaa207032993978395411a173d89be1ad8ddeb5
g.balasubramanian <balasubramaniangopalakrishnan@gmail.com>
2019-02-26 22:26:37 +0530

    solved subset problem

--- src/main/java/com/gb/datastructures/practice/Subsets.java
+++ src/main/java/com/gb/datastructures/practice/Subsets.java
@@ -1,7 +1,30 @@
 package com.gb.datastructures.practice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by gbalasubramanian on 26/02/19.
  */
 public class Subsets {
+    public static List<List<Integer>> subsets(int[] nums) {
+        int len = nums.length;
+        List<List<Integer>> power =  new ArrayList<>();
+        for(int i = 0;i<Math.pow(2,len);i++){
+            List<Integer> res = new ArrayList<>();
+            for (int j =0;j<len;j++) {
+                if((i & (1<<j)) > 0) {
+                    res.add(nums[j]);
+                }
+            }
+            power.add(res);
+        }
+        return power;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3};
+        List<List<Integer>> result = subsets(nums);
+        System.out.println(result);
+    }
 }
