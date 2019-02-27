84f6f9707e68d149acd0688525ab59a1d9047148
g.balasubramanian <balasubramaniangopalakrishnan@gmail.com>
2019-02-27 23:29:26 +0530

    Added driver function

--- src/main/java/com/gb/datastructures/practice/trees/DepthOfTree.java
+++ src/main/java/com/gb/datastructures/practice/trees/DepthOfTree.java
@@ -4,7 +4,7 @@ package com.gb.datastructures.practice.trees;
  * Created by gbalasubramanian on 27/02/19.
  */
 public class DepthOfTree {
-    public int maxDepth(TreeNode root) {
+    public static int maxDepth(TreeNode root) {
         if(root == null)
             return 0;
         int leftDepth = maxDepth(root.left);
@@ -12,4 +12,11 @@ public class DepthOfTree {
         return Math.max(leftDepth, rightDepth) + 1;
     }
 
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(2);
+        root.setLeft(new TreeNode(1));
+        root.setRight(new TreeNode(4));
+        root.getRight().setLeft(new TreeNode(3));
+        System.out.println(maxDepth(root));
+    }
 }
