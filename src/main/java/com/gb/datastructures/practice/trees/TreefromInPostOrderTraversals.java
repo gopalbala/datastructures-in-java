08e83deb6448fa212bbf06a3f9c0f24c52b62670
g.balasubramanian <balasubramaniangopalakrishnan@gmail.com>
2019-03-01 22:20:36 +0530

    added code to construct tree from inorder and postorder traversals

---
+++ src/main/java/com/gb/datastructures/practice/trees/TreefromInPostOrderTraversals.java
@@ -0,0 +1,44 @@
+package com.gb.datastructures.practice.trees;
+
+/**
+ * Created by gbalasubramanian on 28/02/19.
+ */
+public class TreefromInPostOrderTraversals {
+    int index;
+
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        index = postorder.length - 1;
+        return build(inorder, postorder, 0, inorder.length - 1);
+
+    }
+
+    private TreeNode build(int[] ino, int[] po, int start, int end) {
+        if (start > end)
+            return null;
+        TreeNode node = new TreeNode(po[index]);
+        index--;
+        if (start == end)
+            return node;
+        int idx = 0;
+        for (int i = start; i <= end; i++) {
+            if (ino[i] == node.val) {
+                idx = i;
+                break;
+            }
+        }
+        node.right = build(ino, po, idx + 1, end);
+        node.left = build(ino, po, start, idx - 1);
+
+        return node;
+    }
+
+    public static void main(String[] args) {
+        TreefromInPostOrderTraversals treefromInPostOrderTraversals = new TreefromInPostOrderTraversals();
+        int[] in = {9, 3, 15, 20, 7};
+        int[] po = {9, 15, 7, 20, 3};
+        TreeNode node = treefromInPostOrderTraversals.buildTree(in, po);
+        System.out.println(node);
+
+    }
+
+}
