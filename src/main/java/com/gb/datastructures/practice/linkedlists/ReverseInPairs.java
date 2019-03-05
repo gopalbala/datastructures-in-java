4804c760b81b1342bc1c41931f174709f83db3a2
g.balasubramanian <balasubramaniangopalakrishnan@gmail.com>
2019-03-05 23:50:53 +0530

    added reverse node in pairs

---
+++ src/main/java/com/gb/datastructures/practice/linkedlists/ReverseInPairs.java
@@ -0,0 +1,48 @@
+package com.gb.datastructures.practice.linkedlists;
+
+public class ReverseInPairs {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode preHead = new ListNode(0);
+        preHead.next = head;
+        ListNode pre = preHead;
+        ListNode current, next;
+        current = head;
+        next = head.next;
+        while (current != null && next != null) {
+            current.next = next.next;
+            pre.next = next;
+            next.next = current;
+            pre = current;
+            current = current.next;
+            next = current != null ? current.next : null;
+        }
+        return preHead.next;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(val);
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode node = new ListNode(1);
+        node.next = new ListNode(2);
+        node.next.next = new ListNode(3);
+        node.next.next.next = new ListNode(4);
+        ReverseInPairs reverseInPairs = new ReverseInPairs();
+        ListNode n = reverseInPairs.swapPairs(node);
+        System.out.println(n);
+    }
+}
--- src/main/java/com/gb/datastructures/practice/trees/NextRightPointer.java
+++ src/main/java/com/gb/datastructures/practice/trees/NextRightPointer.java
@@ -41,9 +41,9 @@ public class NextRightPointer {
         tree.left = new Node(2);
         tree.right = new Node(3);
         tree.left.left = new Node(4);
-        tree.left.right = new Node(5);
+        //tree.left.right = new Node(5);
 
-        tree.right.left = new Node(6);
+        //tree.right.left = new Node(6);
         tree.right.right = new Node(7);
 
         NextRightPointer nextRightPointer = new NextRightPointer();
