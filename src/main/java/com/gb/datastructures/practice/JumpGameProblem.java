1f7d091cdb50275eeb1a8c4a5c6ec43f35834ddf
g.balasubramanian <balasubramaniangopalakrishnan@gmail.com>
2019-03-10 23:48:26 +0530

    fixed issues and corrected the algo

--- src/main/java/com/gb/datastructures/practice/JumpGameProblem.java
+++ src/main/java/com/gb/datastructures/practice/JumpGameProblem.java
@@ -13,7 +13,7 @@ public class JumpGameProblem {
         while ( nextPos <= nums.length) {
             nextPos += nums[i];
             i += nums[i];
-            if (nextPos == 0)
+            if (i< nums.length && nums[i] == 0)
                 return false;
             if (nextPos >= nums.length)
                 return true;
@@ -22,7 +22,7 @@ public class JumpGameProblem {
     }
 
     public static void main(String[] args) {
-        int[] nums = {2,3,1,1,4};//{3,2,1,0,4};//{2, 3, 1, 1, 4};
+        int[] nums = {3,2,1,0,4};//{2, 3, 1, 1, 4};
         JumpGameProblem jumpGameProblem = new JumpGameProblem();
         boolean res = jumpGameProblem.canJump(nums);
         System.out.println(res);
