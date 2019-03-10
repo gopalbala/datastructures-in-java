f1e571d4607155b646ae86f5de20f13be8a93817
g.balasubramanian <balasubramaniangopalakrishnan@gmail.com>
2019-03-11 00:32:40 +0530

    all test cases passed

--- src/main/java/com/gb/datastructures/practice/JumpGameProblem.java
+++ src/main/java/com/gb/datastructures/practice/JumpGameProblem.java
@@ -2,7 +2,7 @@ package com.gb.datastructures.practice;
 
 public class JumpGameProblem {
     public boolean canJump(int[] nums) {
-        int nextPos = 0;
+        int nextPos = nums[0];
         if (nums.length == 0)
             return false;
         if (nums[0] <= 0)
@@ -10,19 +10,21 @@ public class JumpGameProblem {
         if (nums.length == 1)
             return true;
         int i = 0;
-        while ( nextPos <= nums.length) {
-            nextPos += nums[i];
-            i += nums[i];
-            if (i< nums.length && nums[i] == 0)
+        while (nextPos <= nums.length) {
+            if (nextPos <= i && nums[i] == 0)
                 return false;
-            if (nextPos >= nums.length)
+            if (i + nums[i] > nextPos) {
+                nextPos = i + nums[i];
+            }
+            i++;
+            if (nextPos >= nums.length - 1)
                 return true;
         }
         return false;
     }
 
     public static void main(String[] args) {
-        int[] nums = {3,2,1,0,4};//{2, 3, 1, 1, 4};
+        int[] nums = {3,2,1,0,4};//{3, 0, 8, 2, 0, 0, 1};//{2, 3, 1, 1, 4};//{2,0,0};{2, 5,0, 0};//{2, 3, 1, 1, 4};//{3,2,1,0,4};//{2, 3, 1, 1, 4};
         JumpGameProblem jumpGameProblem = new JumpGameProblem();
         boolean res = jumpGameProblem.canJump(nums);
         System.out.println(res);
